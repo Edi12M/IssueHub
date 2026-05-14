4f52ab24ff9ca54e2f4aa2209c32f9957ef3b264
Nea Metohu <neametohu10@gmail.com>
2026-05-14 09:04:55 +0200

    adding timelog and health services

---
+++ DTOs/TimeLogs/TimeLogResponseDto.cs
@@ -0,0 +1,14 @@
+namespace Backend.DTOs.TimeLogs
+{
+    public class TimeLogResponseDto
+    {
+        public int Id { get; set; }
+        public int IssueId { get; set; }
+        public int UserId { get; set; }
+        public decimal Hours { get; set; }
+        public bool IsBillable { get; set; }
+        public DateTime LogDate { get; set; }
+        public string Note { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
